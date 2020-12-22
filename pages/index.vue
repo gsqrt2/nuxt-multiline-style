e2e217cc8a906b22cc926175d8c2013f7cd8eb28
George Marios <gsqrt2@gmail.com>
2020-12-22 18:54:03 +0200

    multiline style parse error example

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+  class="
+    class1
+    class2
+    class3
+  "
+    style="
+      width: 1264px;
+      z-index: 1;
+      border-radius: 8px;
+      padding: 38px 27px;
+      height: 100px;
+      background-color: white;
+      border: 1px solid black;
+    "
+  >
+    <div
+      class="
+        class1
+        class2
+        class3
+      "
+      style="
+        width: 1264px;
+        z-index: 1;
+        border-radius: 8px;
+        padding: 38px 27px;
+        height: 100px;
+        background-color: white;
+        border: 1px solid black;
+      "
+    >
+      <div
+        class="
+          class1
+          class2
+          class3
+        "
+        style="
+          width: 1264px;
+          z-index: 1;
+          border-radius: 8px;
+          padding: 38px 27px;
+          height: 100px;
+          background-color: white;
+          border: 1px solid black;
+        "
+      >
+        <nuxt-link to="/index_copy">to index copy</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
